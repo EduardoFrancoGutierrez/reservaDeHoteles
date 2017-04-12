72b7eadf366d91551bd3435e93349bc987ad5c17
Enrique Arranz <kikevillab93@gmail.com>
2017-04-12 13:05:00 +0200

    Add bookingTest, HotelTest y RoomTest

--- src/main/java/com/miw/hotel/model/Booking.java
+++ src/main/java/com/miw/hotel/model/Booking.java
@@ -1,6 +1,7 @@
 package com.miw.hotel.model;
 
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class Booking {
 
@@ -18,14 +19,14 @@ public class Booking {
 	
 	public Booking() {}
 
-	public Booking(int id, Calendar startDate, Calendar endDate, Room room, Client client, Status status) {
+	public Booking(int id, Calendar startDate, Calendar endDate, Room room, Client client) {
 		super();
 		this.id = id;
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.room = room;
 		this.client = client;
-		this.status = status;
+		this.status = Status.BOOKING;
 	}
 
 	public int getId() {
@@ -75,6 +76,14 @@ public class Booking {
 	public void setStatus(Status status) {
 		this.status = status;
 	}
+	
+	public boolean isBook() {
+		return this.status == Status.BOOKING;
+	}
+	
+	public long getDuration() {
+		return TimeUnit.MILLISECONDS.toHours(this.endDate.getTimeInMillis() - this.startDate.getTimeInMillis());
+	}
 
 	@Override
 	public String toString() {
@@ -86,7 +95,12 @@ public class Booking {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((client == null) ? 0 : client.hashCode());
+		result = prime * result + ((endDate == null) ? 0 : endDate.hashCode());
 		result = prime * result + id;
+		result = prime * result + ((room == null) ? 0 : room.hashCode());
+		result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
 		return result;
 	}
 
@@ -99,10 +113,34 @@ public class Booking {
 		if (getClass() != obj.getClass())
 			return false;
 		Booking other = (Booking) obj;
+		if (client == null) {
+			if (other.client != null)
+				return false;
+		} else if (!client.equals(other.client))
+			return false;
+		if (endDate == null) {
+			if (other.endDate != null)
+				return false;
+		} else if (!endDate.equals(other.endDate))
+			return false;
 		if (id != other.id)
+			return false;
+		if (room == null) {
+			if (other.room != null)
+				return false;
+		} else if (!room.equals(other.room))
+			return false;
+		if (startDate == null) {
+			if (other.startDate != null)
+				return false;
+		} else if (!startDate.equals(other.startDate))
+			return false;
+		if (status != other.status)
 			return false;
 		return true;
 	}
+
+	
 	
 	
 	

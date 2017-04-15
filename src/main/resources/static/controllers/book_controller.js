169c708893086a76e5fb5b878dcb8a5dfcd5b596
eduardofranco <eduardofranco@localhost>
2017-04-15 14:21:49 +0200

    ui de 8.2.3

--- src/main/resources/static/controllers/book_controller.js
+++ src/main/resources/static/controllers/book_controller.js
@@ -1,9 +1,14 @@
 angular.module(mainApplicationModuleName).controller("BookController", ['hotelService', 'bookService', function(hotelService, bookService) {
 	"use strict";
+	const ERROR_CREATE_BOOK 	= "ERROR AL CREAR UNA RESERVA";
 	
 	var vm = this;
 	
 	vm.hotels = [];
+	vm.showOKCreate=false;
+	vm.showModalBookForm=false;
+	vm.showErrorCreate=false;
+	vm.errorCreate="";
 	
 	vm.getHotels = function() {
 		return vm.requestToGetHotels(hotelService);
@@ -25,6 +30,7 @@ angular.module(mainApplicationModuleName).controller("BookController", ['hotelSe
 		vm.book = {};
 		vm.book.hotel = hotel;
 		vm.book.room = room;
+		vm.showModalBookForm=true;
 	}
 	
 	vm.createBook = function(book) {
@@ -34,6 +40,12 @@ angular.module(mainApplicationModuleName).controller("BookController", ['hotelSe
 	}
 	
 	vm.requestToCreateBook = function(bookService, book) {
-		bookService.create(book);
+		bookService.create(book).then(function(result){
+			vm.showErrorCreate=false;
+		})
+		.catch(function(error){
+			vm.showErrorCreate=true;
+			vm.errorCreate=ERROR_CREATE_BOOK;
+		});
 	}
 }]);
